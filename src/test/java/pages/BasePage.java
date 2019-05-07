e03dbe9ca8a4061885e2185e5f0119517e7d2a00
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-07 08:50:26 +0700

    Implement basic function for each page

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,7 +1,13 @@
 package pages;
 
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
@@ -12,4 +18,71 @@ public class BasePage {
         this.driver = driver;
         this.log = log;
     }
+
+    /**
+     * Open page with given url
+     *
+     * @param url of page to open
+     */
+    protected void openUrl(String url) {
+        driver.get(url);
+    }
+
+    /**
+     * Find element using given locator
+     *
+     * @param locator of web element
+     * @return web element
+     */
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    /**
+     * Click on element with given locator when it's visible
+     *
+     * @param locator of web element
+     */
+    protected void click(By locator) {
+        waitForVisibilityOf(locator, 5);
+        find(locator).click();
+    }
+
+    /**
+     * Type given text into element with given locator
+     *
+     * @param text to type
+     * @param locator of web element
+     */
+    protected void type(String text, By locator) {
+        waitForVisibilityOf(locator, 5);
+        find(locator).sendKeys(text);
+    }
+
+    /**
+     * Wait for given number of seconds for element with given locator to be visible on the page
+     *
+     * @param locator of web element
+     * @param timeOutInSeconds to wait
+     */
+    protected void waitForVisibilityOf(By locator, Integer... timeOutInSeconds) {
+        int attempts = 0;
+        while (attempts < 2) {
+            try {
+                waitFor(ExpectedConditions.visibilityOfElementLocated(locator),
+                        (timeOutInSeconds.length > 0? timeOutInSeconds[0] : null));
+                break;
+            } catch(StaleElementReferenceException e) {
+                e.printStackTrace();
+            }
+            attempts++;
+        }
+    }
+
+    private void waitFor(ExpectedCondition<WebElement> condition, Integer timeOutInSeconds) {
+        timeOutInSeconds = timeOutInSeconds != null ? timeOutInSeconds : 30;
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.until(condition);
+    }
+
 }
