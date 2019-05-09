c72227605605f997738fe1a9feb03b66a629599b
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-09 12:26:54 +0800

    Add Drag and Drop Test suite

---
+++ src/main/java/com/herokuapp/theinternet/pages/DragAndDropPage.java
@@ -0,0 +1,57 @@
+package com.herokuapp.theinternet.pages;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DragAndDropPage extends BasePageObject {
+
+    private String pageUrl = "http://the-internet.herokuapp.com/drag_and_drop";
+
+    private By columnA = By.id("column-a");
+    private By columnB = By.id("column-b");
+
+    public DragAndDropPage(WebDriver driver, Logger log) {
+        super(driver, log);
+    }
+
+    /**
+     * Open DragAndDropPage with it's url
+     */
+    public void openPage() {
+        log.info("Opening page: " + pageUrl);
+        openUrl(pageUrl);
+        log.info("Page opened!");
+    }
+
+    /**
+     * Drag A box and drop it on B box
+     */
+    public void dragAtoB() {
+        log.info("Drag and drop A box on B box");
+        performDragAndDrop(columnA, columnB);
+    }
+
+    /**
+     * Getting Column A Text
+     *
+     * @return column A text
+     */
+    public String getColumnAText() {
+        String text = find(columnA).getText();
+        log.info("Column A Text: " + text);
+        return text;
+    }
+
+    /**
+     * Getting column B Text
+     *
+     * @return
+     */
+    public String getColumnBText() {
+        String text = find(columnB).getText();
+        log.info("Column B Text: " + text);
+        return text;
+    }
+
+}
---
+++ src/test/java/com/herokuapp/theinternet/draganddroptests/DragAndDropTests.java
@@ -0,0 +1,39 @@
+package com.herokuapp.theinternet.draganddroptests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.DragAndDropPage;
+
+public class DragAndDropTests extends TestUtilities {
+
+    @Test
+    public void dragAToBTest() {
+
+        log.info("Starting dragAToBTest");
+
+        //---------------------------------------------------------------
+        // EXECUTION
+        //---------------------------------------------------------------
+
+        // Open DragAndDropPage
+        DragAndDropPage dragAndDropPage = new DragAndDropPage(driver, log);
+        dragAndDropPage.openPage();
+
+        // Drag box A and drop it on box B
+        dragAndDropPage.dragAtoB();
+
+        //---------------------------------------------------------------
+        // VERIFICATIONS
+        //---------------------------------------------------------------
+
+        // Verify correct headers in correct boxes
+
+        String columnAText = dragAndDropPage.getColumnAText();
+        Assert.assertTrue(columnAText.equals("B"), "Column A header should be B, but it is: " + columnAText);
+
+        String columnBText = dragAndDropPage.getColumnBText();
+        Assert.assertTrue(columnBText.equals("A"), "Column A header should be B, but it is: " + columnBText);
+    }
+}
