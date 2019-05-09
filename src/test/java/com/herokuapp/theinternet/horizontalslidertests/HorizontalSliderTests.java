821c2702cc16629ee6fd71f661528a7684a3f0fc
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-09 12:16:07 +0800

    Add Horizontal Slider Test

---
+++ src/test/java/com/herokuapp/theinternet/horizontalslidertests/HorizontalSliderTests.java
@@ -0,0 +1,39 @@
+package com.herokuapp.theinternet.horizontalslidertests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.HorizontalSliderPage;
+
+public class HorizontalSliderTests extends TestUtilities {
+
+    @Test
+    public void sliderTest() {
+
+        log.info("Starting sliderTest");
+
+        //---------------------------------------------------------------
+        // EXECUTION
+        //---------------------------------------------------------------
+
+        // Open HorizontalSliderPage
+        HorizontalSliderPage horizontalSliderPage = new HorizontalSliderPage(driver, log);
+        horizontalSliderPage.openPage();
+
+        String value = "3.5";
+
+        // Set slider value
+        sleep(2000);
+        horizontalSliderPage.setSliderTo(value);
+        sleep(2000);
+
+        //---------------------------------------------------------------
+        // VERIFICATIONS
+        //---------------------------------------------------------------
+
+        // Verify slider value
+        String sliderValue = horizontalSliderPage.getSliderValue();
+        Assert.assertTrue(sliderValue.equals(value), "Range is not correct. It is: " + sliderValue);
+    }
+}
