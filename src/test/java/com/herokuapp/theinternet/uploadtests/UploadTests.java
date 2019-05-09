08e8e0e31a6db0fc2ab2d0810a2e5994c94f1c57
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-09 12:07:10 +0800

    Add Upload File Test Suite

---
+++ src/test/java/com/herokuapp/theinternet/uploadtests/UploadTests.java
@@ -0,0 +1,42 @@
+package com.herokuapp.theinternet.uploadtests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.FileUploaderPage;
+
+public class UploadTests extends TestUtilities {
+
+    @Test
+    public void imageUploadTest() {
+
+        log.info("Starting imageUploadTest");
+
+        //---------------------------------------------------------------
+        // EXECUTION
+        //---------------------------------------------------------------
+
+        // open File Uploader Page
+        FileUploaderPage fileUploaderPage = new FileUploaderPage(driver, log);
+        fileUploaderPage.openPage();
+
+        // Select file
+        String fileName = "logo.png";
+        fileUploaderPage.selectFile(fileName);
+
+        // Push upload button
+        fileUploaderPage.pushUploadButton();
+
+        //---------------------------------------------------------------
+        // VERIFICATIONS
+        //---------------------------------------------------------------
+
+        // Get uploaded files
+        String fileNames = fileUploaderPage.getUploadedFilesNames();
+
+        // Verify selected file uploaded
+        Assert.assertTrue(fileNames.contains(fileName),
+                "Our file (" + fileName + ") is not one of the uploaded (" + fileNames + ")");
+    }
+}
